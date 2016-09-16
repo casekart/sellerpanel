5f201825b4218ba997382b687312d655ba406404
Ganeshkumar275 <ganeshkumar.t.j.b@gmail.com>
2016-09-16 10:07:30 +0530

    download stock page  created
    download stock page  created

--- application/controllers/Download_stock.php
+++ application/controllers/Download_stock.php
@@ -6,11 +6,22 @@ class Download_stock extends CI_Controller
 		if (!$this->session->userdata('user_id')) {
 			$this->load->view('login');
 		}else{
-			$this->load->view('download_stock');
+
+			$data['user_portal'] = $this->mongo_db->get('user_portal');
+			$this->load->view('download_stock',$data);
+			// $this->load->view('download_stock');
 		}
 	}
-// 	public function table()
-// {
-
-// }
+	public function table()
+	{
+		$data['user_portal'] = $this->mongo_db->get('user_portal');
+		$this->load->view('download_stock',$data);
+	}
+	public function change_status()
+	{
+		$condition = array('_id' => new MongoId($id));
+		$data = array('city' => 'surat');
+		$this->mongo_db->where($condition);
+		$this->mongo_db->update('user',$data);
+	}
 }
--- application/views/Download_stock.php
+++ application/views/Download_stock.php
@@ -1,25 +1,104 @@
 <?php include_once("page_header.php"); ?>
 <div class="row">
   <div class="col-sm-12 col-xs-12 col-md-12 col-lg-12 main">
-    <table id="dataTables-example">
-         <thead>
-                <tr>
-                    <th>ClientName</th>
-                    <th>SKU code</th>
-                    <th>Design code</th>
-                    <th>Model Name</th>
-                    <th>Status</th>
-                    <th>Quantity</th>
-                    <th>MRP</th>
-                    <th>Sales Price</th>
-                </tr>
-            </thead>
-    </table>
-  </div>
+    <table id="example" class="table table-striped table-bordered">
+     <thead>
+      <tr>
+        <th>Select</th>
+        <th>ClientName</th>
+        <th>Portal</th>
+        <th>MasterCode</th>
+        <th>Model Name</th>
+        <th>Status</th>
+        <th>Quantity</th>
+        <th>Group</th>
+        <th>MRP</th>
+        <th>Sales Price</th>
+        <th><button>Save All</button></th>
+      </tr>
+    </thead>
+    <?php foreach ($user_portal as $changes):?>
+    <tr>
+      <td><input type="checkbox" name="check[]" id="userid" value="<?php echo $changes['UserID']?>"/></td>
+      <td><?php echo $changes['UserID']?></td>
+      <td><?php echo $changes['Portal']?></td>
+      <td><?php echo $changes['MasterCode']?></td>
+      <td><?php echo $changes['ModelName']?></td>
+      <td>
+        <label class="switch">
+          <input type="checkbox" id="status" status="" value="<?php echo $changes['Status']?>">
+          <div class="slider round"></div>
+        </label>
+      </td>
+      <td><input type="text" id="qty" qty="" value="<?php echo $changes['Quantity']?>"></td>
+      <td><?php echo $changes['Group']?></td>
+      <td><?php echo $changes['MRP']?></td>
+      <td><input type="text" id="price" price="" value="<?php echo $changes['SellingPrice']?>"></td>
+      <td><button id="submit">Save</button></td>
+    </tr>
+  <?php endforeach?>
+</table>
+</div>
 </div>
 <?php include_once("scripts.php"); ?>
 <script type="text/javascript">
-$(document).ready( function () {
-  $('#dataTables-example').DataTable();
-} );
+$(document).ready(function(){
+
+$('table tr #status').each(function() {
+       var elem = $(this);
+
+       elem.data('oldVal', elem.val());
+       elem.data('staVal', elem.val());
+
+       // Look for changes in the value
+       elem.bind("propertychange change", function(event){
+          if (elem.data('oldVal') == "active") {
+            elem.data('oldVal', "inactive")
+          }else{
+            elem.data('oldVal', "active")
+          }
+          elem.val(elem.data('oldVal'));
+          // If value has changed...
+          if (elem.data('staVal') != elem.val()) {
+           // Updated change value
+           elem.closest('tr').find('input#userid').prop('checked', true);
+           elem.closest('tr').find('button#submit').css('background-color',"yellow");
+         }
+         else{
+            if (elem.closest('tr').find('input#userid').is(":checked")) {
+          elem.closest('tr').find('input#userid').prop('checked', false);
+          elem.closest('tr').find('button#submit').css('background-color',"");
+        }
+         }
+       });
+     });
+
+
+$('table tr #qty, table tr #price').each(function() {
+       var elem = $(this);
+
+       // Save current value of element
+       elem.data('oldVal', elem.val());
+       elem.data('staVal', elem.val());
+
+       // Look for changes in the value
+       elem.bind("propertychange change click keyup input paste", function(event){
+          // If value has changed...
+          if (elem.data('oldVal') != elem.val() && elem.data('staVal') != elem.val()) {
+           // Updated stored value
+           elem.data('oldVal', elem.val());
+           elem.closest('tr').find('input#userid').prop('checked', true);
+           elem.closest('tr').find('button#submit').css('background-color',"yellow");
+         }
+         else if(elem.data('oldVal') != elem.val()){
+          elem.data('oldVal', elem.val());
+          if (elem.closest('tr').find('input#userid').is(":checked")) {
+            elem.closest('tr').find('input#userid').prop('checked', false);
+            elem.closest('tr').find('button#submit').css('background-color',"");
+          }
+         }
+      });
+     });
+
+});
 </script>
